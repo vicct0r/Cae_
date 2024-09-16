f3d939a49ac968b0126ffadf4aa94f9cb79b45f7
vicct0r <v1ctor_inacio@proton.me>
2024-09-16 01:42:44 -0300

    Corrigindo para produção

--- projeto_cae/settings.py
+++ projeto_cae/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^vcr(5wc*v=foepb#0$)5#q_19kkn-r(l=pt*%hli7dn24n)6e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'sistema-cae-68509a198bf7.herokuapp.com',  # Domínio do Heroku (produção)
